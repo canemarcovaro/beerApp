508bf77d6cdc3b3f1f419bb5a6d7331d5365fb43
Marco <marcocanevaro@hotmail.com>
2020-01-24 17:11:17 -0300

    Agregado de  Role en user en DB

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 //Rutas para las Cervecerias.
-
+Route::group(['middleware' => 'auth:api'], function() {
+    
 Route::get('breweries','BrewerieController@getAll')->name('getAllBreweries');
-
 Route::post('breweries','BrewerieController@add')->name('addBreweries');
-
 Route::get('breweries/{id}','BrewerieController@get')->name('getBreweries');
-
 Route::post('breweries/{id}','BrewerieController@edit')->name('editBreweries');
 
 //Rutas para las Ofertas.
@@ -30,6 +28,13 @@ Route::get('offers','OfferController@getAll')->name('getAllOffers');
 Route::post('offers','OfferController@add')->name('addOffer');
 Route::get('offers/{id}','OfferController@get')->name('getOffer');
 Route::post('offers/{id}','OfferController@edit')->name('editOffer');
+
+});
+
+
+
+
+
 
 //Rutas de auth.
 
