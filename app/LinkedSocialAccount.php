2d2ae1a7db9b8fd6a06688dff2f99f97b07cde98
Marco <marcocanevaro@hotmail.com>
2020-01-26 17:28:25 -0300

    Migraciones, create lnked social accounts table y  change password and email fields nullable.

---
+++ app/LinkedSocialAccount.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
+class LinkedSocialAccount extends Model
+{
+    protected $fillable = [
+        'provider_name',
+        'provider_id',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+}
