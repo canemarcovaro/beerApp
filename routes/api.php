0b59d1e8699a7ba8648790954d4041a1d82b8961
Marco <marcocanevaro@hotmail.com>
2020-01-28 19:59:45 -0300

    Cambio en rutas para pruebas

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 //Rutas para las Cervecerias.
-Route::group(['middleware' => ['auth:api','scope:add-brewery']], function() {
+/*Route::group(['middleware' => ['auth:api','scope:add-brewery']], function() {
     
 Route::get('breweries','BrewerieController@getAll')->name('getAllBreweries');
 Route::post('breweries','BrewerieController@add')->name('addBreweries');
@@ -29,10 +29,24 @@ Route::post('offers','OfferController@add')->name('addOffer');
 Route::get('offers/{id}','OfferController@get')->name('getOffer');
 Route::post('offers/{id}','OfferController@edit')->name('editOffer');
 
-});
+});*/
 
 
-
+Route::group(['middleware' => 'auth:api'], function() {
+    
+    Route::get('breweries','BrewerieController@getAll')->name('getAllBreweries');
+    Route::post('breweries','BrewerieController@add')->name('addBreweries');
+    Route::get('breweries/{id}','BrewerieController@get')->name('getBreweries');
+    Route::post('breweries/{id}','BrewerieController@edit')->name('editBreweries');
+    
+    //Rutas para las Ofertas.
+    
+    Route::get('offers','OfferController@getAll')->name('getAllOffers');
+    Route::post('offers','OfferController@add')->name('addOffer');
+    Route::get('offers/{id}','OfferController@get')->name('getOffer');
+    Route::post('offers/{id}','OfferController@edit')->name('editOffer');
+    
+    });
 
 
 
