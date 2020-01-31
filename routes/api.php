f6b977a084ae6a71e082a466c3710dfc324466de
Marco Canevaro <marcocanevaro@hotmail.com>
2020-01-30 23:43:00 -0300

    refactoring rutas y comentarios.

--- routes/api.php
+++ routes/api.php
@@ -14,20 +14,36 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Rutas para las Cervecerias.
+//Rutas de auth para el brewery owner.
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+  
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
+
+//Rutas de brewery owner.
 Route::group(['middleware' => ['auth:api','scope:brewery-owner']], function() {
-    
-Route::get('breweries','BrewerieController@getAll')->name('getAllBreweries');
+//Rutas para las Cervecerias.
+//ALTA de una cervceria x brewery owner.
 Route::post('breweries','BrewerieController@add')->name('addBreweries');
-Route::get('breweries/{id}','BrewerieController@get')->name('getBreweries');
+//Edit de la cerveceria del brewery owner.
 Route::post('breweries/{id}','BrewerieController@edit')->name('editBreweries');
-
+//Listado de las cervecerias.
+Route::get('breweries','BrewerieController@getAll')->name('getAllBreweries');
+Route::get('breweries/{id}','BrewerieController@get')->name('getBreweries');
+//--------------------------------------------------------------------------------------------
 //Rutas para las Ofertas.
-
-Route::get('offers','OfferController@getAll')->name('getAllOffers');
+//Alta de oferta de una cerveceria (por el momento numero indeterminado).
 Route::post('offers','OfferController@add')->name('addOffer');
-Route::get('offers/{id}','OfferController@get')->name('getOffer');
+//Edit de la oferta seleccionada.
 Route::post('offers/{id}','OfferController@edit')->name('editOffer');
+//Listado de las ofertas.
+Route::get('offers','OfferController@getAll')->name('getAllOffers');
+Route::get('offers/{id}','OfferController@get')->name('getOffer');
 
 });
 
@@ -49,15 +65,3 @@ Route::group(['middleware' => 'auth:api'], function() {
     });
 
 */
-
-//Rutas de auth.
-
-Route::group(['prefix' => 'auth'], function () {
-    Route::post('login', 'AuthController@login');
-    Route::post('signup', 'AuthController@signup');
-  
-    Route::group(['middleware' => 'auth:api'], function() {
-        Route::get('logout', 'AuthController@logout');
-        Route::get('user', 'AuthController@user');
-    });
-});
