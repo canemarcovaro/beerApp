beda3587ab0354e404a7c088fa85456edc6976c0
Marco Canevaro <marcocanevaro@hotmail.com>
2020-02-07 00:41:09 -0300

    Creacion de oferta por una cerveceria

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Offer;
+use App\Brewerie;
 
 class OfferController extends Controller
 {
@@ -14,8 +15,19 @@ class OfferController extends Controller
     }
 
     public function add(Request $request){
-    $offer = Offer::create($request->all());
+    
+    $user = $request->user();
+    
+    $brewery = Brewerie::where('user_id',$user->id)->first();
+    
+    $offer = new Offer;
+    $offer->name = $request->name;
+    $offer->detail = $request->detail;
+    $offer->brewery_id=$brewery->id;
+    $offer->save();
+
     return $offer;
+
     }
 
     public function get($id){
